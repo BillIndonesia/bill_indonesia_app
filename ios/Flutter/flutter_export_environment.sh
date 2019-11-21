ad6f22e95bc60e5cd338100d97c903bf652a8637
reza zesariana <rezazesariana58@gmail.com>
2019-11-21 13:31:23 +0700

    changes ios file

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/desktop2/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/desktop2/Documents/bitbucket folder/bill"
+export "FLUTTER_ROOT=/Users/appledeveloperekabill.co.id/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/appledeveloperekabill.co.id/Documents/bill git"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/desktop2/Documents/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/appledeveloperekabill.co.id/Documents/flutter/bin/cache/artifacts/engine/ios-release"
 export "FLUTTER_BUILD_NAME=0.7.3"
 export "FLUTTER_BUILD_NUMBER=22"

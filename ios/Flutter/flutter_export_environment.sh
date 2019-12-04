445a89e95e1781817a2451b86565680a95e10d97
Muhammad Vikral <Mvikral79@gmail.com>
2019-12-04 10:11:09 +0700

    back ga boleh

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/desktop2/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/desktop2/Documents/bitbucket/bill"
+export "FLUTTER_ROOT=/home/vikral/dev/flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/vikral/dev/flutter/bill master"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/desktop2/Documents/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=0.7.4"
-export "FLUTTER_BUILD_NUMBER=24"
+export "FLUTTER_FRAMEWORK_DIR=/home/vikral/dev/flutter/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=0.7.5"
+export "FLUTTER_BUILD_NUMBER=25"
